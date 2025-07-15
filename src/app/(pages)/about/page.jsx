585428982a034b01975b4b047298a394e8dcb160
EduardoVizicato <eduardovizicato@gmail.com>
2025-07-15 14:01:07 -0300

    starting contact page

--- src/app/(pages)/about/page.jsx
+++ src/app/(pages)/about/page.jsx
@@ -7,6 +7,20 @@ const About = () => {
       <Navbar />
       <div id='about' className='w-full px[12%] py-10'>
         <h2 className='py-10 text-center mb-2 text-3xl'>About me</h2>
+
+        <div className='flex w-full flex-col lg:flex-row text-center'>
+          <div className='flex-1'>
+            <p className='mb-10'>
+              I am a backend developer with over 2 year of expertise in the field. Throughout my studies,
+              I had the privilege of collaborating with prestigious people, contributing and learning new technologies.
+            </p>
+          </div>
+        </div>
+
+        <ul>
+          
+        </ul>
+
       </div>
     </>
   )
--- src/app/(pages)/contact/page.jsx
+++ src/app/(pages)/contact/page.jsx
@@ -1,11 +1,82 @@
+import Navbar from '@/app/components/navbar'
 import React from 'react'
 
-const page = () => {
+const Contact = () => {
   return (
-    <div>
-      <h1>Contact Page</h1>
-    </div>
+    <>
+      <Navbar />
+      <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div
+          aria-hidden="true"
+          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+        >
+        </div>
+        
+        <div className="mx-auto max-w-2xl text-center">
+          <h2 className="text-4xl font-semibold tracking-tight text-balance text-gray-900 sm:text-5xl">Contact me</h2>
+          <p className="mt-2 text-lg/8 text-gray-600">Please ensure you have filled all the gaps</p>
+        </div>
+        
+        <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
+          <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+            <div className="">
+              <label htmlFor="first-name" className="block text-sm/6 font-semibold text-gray-900">
+                First name
+              </label>
+              <div className="mt-2.5">
+                <input
+                  id="first-name"
+                  name="first-name"
+                  type="text"
+                  autoComplete="given-name"
+                  className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                />
+              </div>
+            </div>
+            
+            <div className="">
+              <label htmlFor="email" className="block text-sm/6 font-semibold text-gray-900">
+                Email
+              </label>
+              <div className="mt-2.5">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                />
+              </div>
+            </div>
+            
+            <div className="sm:col-span-2">
+              <label htmlFor="message" className="block text-sm/6 font-semibold text-gray-900">
+                Message
+              </label>
+              <div className="mt-2.5">
+                <textarea
+                  id="message"
+                  name="message"
+                  rows={4}
+                  className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                  defaultValue={''}
+                />
+              </div>
+            </div>
+            
+          </div>
+          <div className="mt-10">
+            <button
+              type="submit"
+              className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Let's talk
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
   )
 }
 
-export default page
+export default Contact
